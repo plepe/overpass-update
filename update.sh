d2934ab10a2608d4154d84bb647e831305895f19
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-08-06 07:28:18 +0200

    Before downloading, check if new file is online -> if not, try again regularily

--- update.sh
+++ update.sh
@@ -4,11 +4,30 @@ cd
 mkdir db-new
 DB_DIR="/home/overpass/db-new"
 PLANET="/home/overpass/planet-latest.osm.bz2"
+TIMESTAMP="/home/overpass/last_planet_date"
 
+LAST_OSMDATE=`cat ${TIMESTAMP}`
+
+#echo $LAST_TIMESTAMP
+#
+#exit
 date
+
+while : ; do
+# check file date on the server
+  OSMDATE=`curl -I http://planet.openstreetmap.org/planet/planet-latest.osm.bz2  | grep -e '^Location:' | sed 's/^.*planet-\(.*\)\.osm\.bz2.*$/\1/'`
+  echo "Newest planet file date: ${OSMDATE}, current: ${LAST_OSMDATE}"
+
+# if this is a new file, continue with the rest of the code
+  [[ "${LAST_OSMDATE}" -ge "${OSMDATE}" ]] || break
+
+# otherwise wait for half an hour and try again
+  sleep 900
+done
 
 rm $PLANET
 time wget --progress=dot:giga -O $PLANET http://planet.openstreetmap.org/planet/planet-latest.osm.bz2
+touch -r planet-latest.osm.bz2 last_planet_timestamp
 
 date
 
@@ -36,5 +55,6 @@ date
 mv db-new/* db/
 
 date
+echo ${OSMDATE} > ${TIMESTAMP}
 
 #touch /tiles/planet-import-complete
